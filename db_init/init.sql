77251bec2b813f01b731994fb3787215b374ae3a
justynlarry <justynlarry@gmail.com>
2025-10-24 14:59:35 -0600

    --

--- db_init/init.sql
+++ db_init/init.sql
@@ -1,5 +1,3 @@
-    CREATE DATABASE IF NOT EXISTS infralogDb01;
-    USE infralogDb01;
 
     CREATE TABLE IF NOT EXISTS log_table (
         id INT AUTO_INCREMENT PRIMARY KEY,
@@ -7,7 +5,7 @@
         message VARCHAR(255) NOT NULL,
         hostname VARCHAR(25) NOT NULL,
 	command_name VARCHAR(75) NOT NULL,
-	process_id INT(15),
-	priority INT(2) NOT NULL,
-        syslog_id VARCHAR(15)
+	process_id INT,
+	priority INT NOT NULL,
+        syslog_id VARCHAR(15) NOT NULL
     );
