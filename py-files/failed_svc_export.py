d3d8c492ac69aa986b953ed3e67a6897dff82502
justynlarry <justynlarry@gmail.com>
2025-11-24 12:41:14 -0700

    Created failed_svc_export.py and parse_failed_svc.py to parse and export the Failed Services section of the vm system reports.

---
+++ py-files/failed_svc_export.py
@@ -0,0 +1,47 @@
+import os
+from parse_failed_svc import parse_failed_svc, TARGET_KEYS
+from mysql_export import insert_records
+from datetime import datetime
+
+TABLE_NAME = "failed_svcs"
+
+COLUMN_MAPPING = {
+    'UNIT':         'unit',
+    'LOAD':         'load',
+    'ACTIVE':       'active',
+    'SUB':          'sub',
+    'DESCRIPTION':  'description'
+}
+
+DB_COLUMNS = [
+    "unit",
+    "load",
+    "active",
+    "sub",
+    "description",
+    "report_host",
+    "report_date",
+    "report_uuid"
+]
+
+def main():
+    
+    today = datetime.today().strftime("%Y-%m-%d")
+    filename = f"../../vm_system_reports/vm_system_report_{today}.log"
+
+    records = parse_failed_svc(filename)
+
+    print(f"Parsed {len(records)} structured log entries.")
+
+    rows = []
+    for rec in records:
+        row = tuple(
+            rec.get(parsed_key)
+            for parsed_key in TARGET_KEYS + ["REPORT_HOST", "REPORT_DATE", "REPORT_UUID"]
+        )
+        rows.append(row)
+
+    insert_records(TABLE_NAME, DB_COLUMNS, rows)
+
+if __name__ == "__main__":
+    main()
---
+++ py-files/parse_failed_svc.py
@@ -0,0 +1,69 @@
+import re
+from parse_metadata import extract_host_metadata
+from datetime import datetime
+
+# Define Target Keys:
+TARGET_KEYS = [
+    'UNIT',
+    'LOAD',
+    'ACTIVE',
+    'SUB',
+    'DESCRIPTION'
+]
+
+def parse_failed_svc(filename):
+    """
+    Parse Failed Services information from the vm_system_report
+    Returns a list of dictionaries with storage data
+    """
+
+    with open(filename, "r") as f:
+        lines = f.readlines()
+    # Split into blocks by host
+    blocks = []
+    current = []
+
+    for line in lines:
+        if line.startwith('--- Host:') and current:
+            blocks.append(current)
+            current = []
+        current.append(line)
+
+    if current:
+        blocks.append(current)
+
+    all_records = []
+
+    for block in blocks:
+        hostname, report_date, uuid = extract_host_metadata(block)
+
+        try:
+            start_idx = next(
+                i for i, l in enumerate(block)
+                if l.startswith('===BEGIN:FAILED_SERVICES===')
+            )
+            end_idx = next(
+                i for i, l in enumerate(block)
+                if l.startswith('===END:FAILED_SERVICES==')
+            )
+        except StopIteration:
+            continue
+        storage_lines = block[start_idx + 1:end_idx]
+
+        for line in storage_lines:
+            line=line.rstrip()
+
+            if not line or line.startswith('UNIT'):
+                continue
+
+            fields = line.split()
+            if len(fields) >=5:
+                record = {
+                    "UNIT":         fields[0],
+                    "LOAD":         fields[1],
+                    "ACTIVE":       fields[2],
+                    "SUB":          fields[3],
+                    "DESCRIPTION":  fields[4]
+                }
+                all_records.append(record)
+    return all_records
